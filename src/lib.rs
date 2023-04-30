2a70f852c01e8081ebd214e34ce9bd6f1d1d2ef7
Fitti <dev@fitti.io>
2023-04-30 18:30:32 +0200

    Start basic testing structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(missing_docs, clippy::missing_errors_doc)]
+use std::{error, fmt};
 
 use worker::{event, Context, Env, Request, Response, Router};
 
@@ -11,7 +12,35 @@ pub async fn main(req: Request, env: Env, _ctx: Context) -> worker::Result<Respo
     let router = Router::new();
 
     router
-        .get("/", |_, _| Response::ok("Hello, pond!"))
+        .get("/", |_req, _ctx| match webhook() {
+            Ok(body) => Response::ok(body),
+            Err(err) => Response::error(err.to_string(), 500),
+        })
         .run(req, env)
         .await
+}
+
+pub fn webhook() -> Result<String, WebhookError> {
+    Ok("Hello, world!".to_string())
+}
+
+#[derive(Debug)]
+pub enum WebhookError {}
+
+impl error::Error for WebhookError {}
+
+impl fmt::Display for WebhookError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    #[test]
+    fn webhook_ok() {
+        let _response = webhook().unwrap();
+    }
 }
