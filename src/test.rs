958593254792b0aadab362151c171d9f8ccbd473
Fitti <dev@fitti.io>
2023-05-04 13:46:05 +0200

    Add notifiaction and revocation tests

--- src/test.rs
+++ src/test.rs
@@ -28,8 +28,8 @@ fn responds_to_challenge() {
             "version": "1",
             "cost": 1,
             "condition": {
-              "broadcaster_user_id": "12826",
-              "reward_id": "239847"
+                "broadcaster_user_id": "12826",
+                "reward_id": "239847"
             },
             "transport": {
               "method": "webhook",
@@ -41,7 +41,70 @@ fn responds_to_challenge() {
     let mut headers = headers();
     headers.message_type = notifications::MessageType::WebhookCallbackVerification;
     headers.signature =
-        "sha256=8bb55a7fc20b88cc2732dd220cb3bd559012eab29c2ae254724ad475d3daee01".to_string();
+        "sha256=10ec72721a3462ff6e82bdfdb837973cfd5a8a81e664c3c8b40974ee7ff69b55".to_string();
     let response = webhook("callback", &headers, challenge).unwrap();
     assert_eq!(response, "pogchamp-kappa-360noscope-vohiyo".to_string());
+}
+
+#[test]
+fn responds_to_notifiaction() {
+    let challenge = r#"{
+        "subscription": {
+            "id": "f1c2a387-161a-49f9-a165-0f21d7a4e1c4",
+            "status": "enabled",
+            "type": "channel.channel_points_custom_reward_redemption.add",
+            "version": "1",
+            "cost": 1,
+            "condition": {
+                "broadcaster_user_id": "12826",
+                "reward_id": "239847"
+            },
+            "transport": {
+                "method": "webhook",
+                "callback": "https://example.com/webhooks/callback"
+            },
+            "created_at": "2019-11-16T10:11:12.634234626Z"
+        },
+        "event": {
+            "user_id": "1337",
+            "user_login": "awesome_user",
+            "user_name": "Awesome_User",
+            "broadcaster_user_id":     "12826",
+            "broadcaster_user_login":  "twitch",
+            "broadcaster_user_name":   "Twitch",
+            "followed_at": "2020-07-15T18:16:11.17106713Z"
+        }
+    }"#;
+    let mut headers = headers();
+    headers.message_type = notifications::MessageType::WebhookCallbackVerification;
+    headers.signature =
+        "sha256=ddcf1796ba64df4e6f911e412cce7b37733a43b71b61c58b80ff25a972b31cbf".to_string();
+    webhook("notification", &headers, challenge).unwrap();
+}
+
+#[test]
+fn responds_to_revocation() {
+    let challenge = r#"{
+        "subscription": {
+            "id": "f1c2a387-161a-49f9-a165-0f21d7a4e1c4",
+            "status": "authorization_revoked",
+            "type": "channel.channel_points_custom_reward_redemption.add",
+            "cost": 1,
+            "version": "1",
+            "condition": {
+                "broadcaster_user_id": "12826",
+                "reward_id": "239847"
+            },
+            "transport": {
+                "method": "webhook",
+                "callback": "https://example.com/webhooks/callback"
+            },
+            "created_at": "2019-11-16T10:11:12.634234626Z"
+        }
+    }"#;
+    let mut headers = headers();
+    headers.message_type = notifications::MessageType::WebhookCallbackVerification;
+    headers.signature =
+        "sha256=e952190b726d6100863096e5982a899f2f70506b01edba8203eba3b80c2a2525".to_string();
+    webhook("revocation", &headers, challenge).unwrap();
 }
