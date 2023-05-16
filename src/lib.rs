ae3c3152d1cc015be18f16a3541775625eabf69f
Fitti <dev@fitti.io>
2023-05-16 11:48:12 +0200

    Include user_id in SE Token form

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(missing_docs, clippy::missing_panics_doc, clippy::missing_errors_doc)]
 
-use worker::{event, Context, Env, Request, Response, Router, Url};
+use worker::{event, Context, Env, FormEntry, Request, Response, Router, Url};
 
 mod apis;
 mod data;
@@ -40,16 +40,14 @@ pub async fn main(req: Request, env: Env, _ctx: Context) -> worker::Result<Respo
                     let users = ctx.kv("users")?;
                     let mut user = User::from_login(token, token_metadata);
                     let mut redirect: Url = req.url()?;
-                    redirect.set_query(None);
+                    let query = format!("user_id={}", user.token_metadata.user_id);
+                    redirect.set_query(Some(&query));
                     if user.with_se_token(&users).await.is_some() {
                         users
                             .put(&user.token_metadata.user_id, &user)?
                             .execute()
                             .await?;
-                        redirect.set_path(&format!(
-                            "/authenticate?token={}",
-                            user.token_metadata.user_id
-                        ));
+                        redirect.set_path("/authenticate");
                     } else {
                         redirect.set_path("/se_token");
                     }
@@ -63,9 +61,32 @@ pub async fn main(req: Request, env: Env, _ctx: Context) -> worker::Result<Respo
         .get("/authenticate", |req, _ctx| {
             Response::ok(format!("This is how you got here: {}", req.url().unwrap()))
         })
-        .get("/se_token", |_req, _ctx| {
-            let html = html::SE_TOKEN_FORM;
+        .get("/se_token", |req, _ctx| {
+            let url = req.url()?;
+            let Some(user_id) = url.query_pairs().find_map(|(k, v)| {
+                if k == "user_id" {
+                    Some(v)
+                } else {
+                    None
+                }
+            }) else {
+                return Response::error("Missing user_id", 400);
+            };
+            let html = html::se_token_form(&user_id);
             Response::from_html(html)
+        })
+        .post_async("/se_token", |mut req, _ctx| async move {
+            let data = req.form_data().await?;
+            let (
+                    Some(FormEntry::Field(se_token)),
+                    Some(FormEntry::Field(user_id))
+                ) = (
+                    data.get("se_token"),
+                    data.get("user_id")
+                ) else {
+                return Response::error("Bad form data", 400)
+            };
+            Response::ok(format!("SE Token: {}, User ID: {}", se_token, user_id))
         })
         .post_async("/eventsub", |mut req, ctx| async move {
             let headers: &TwitchHeaders = &req.headers().try_into()?;
